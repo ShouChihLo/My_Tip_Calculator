1095db27e02819045df90e201178e53e47e0c078
sclo <sclo@gms.ndhu.edu.tw>
2022-10-25 21:32:48 +0800

    second commit

--- app/src/main/java/com/example/mytipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/mytipcalculator/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.mytipcalculator
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.mytipcalculator.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -20,11 +24,20 @@ class MainActivity : AppCompatActivity() {
             calculateTip()
         }
 
+        binding.costOfService.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
+
     }
 
     private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+
         val selectedId = binding.tipOptions.checkedRadioButtonId
         val tipPercentage = when (selectedId) {
             R.id.option_twenty_percent -> 0.20
@@ -40,8 +53,18 @@ class MainActivity : AppCompatActivity() {
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
 
         // Tip Amount: %s
+    }
 
-
+    // Hide keyboard on Enter key
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 
 }
